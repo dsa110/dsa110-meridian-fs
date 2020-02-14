27655f00595a00256a5aaaa552a8af29cff9b31c
Dana Simard <dana.simard@astro.caltech.edu>
2020-02-14 14:41:49 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='dsa110-calib',
-      version='0.2',
+setup(name='dsa110-meridian-fs',
+      version='0.1',
       url='http://github.com/dsa110/dsa110-calib/',
       author='Dana Simard',
       author_email='dana.simard@astro.caltech.edu',
-      packages=['dsacalib'],
-      package_data={'dsacalib':['data/*.all','data/templatekcal']},
-      requirements=['casa-python','casa-data','astropy','scipy','psrdada-python','h5py'],
+      packages=['dsamfs'],
+      package_data={'dsamfs':['data/*.all','data/templatekcal']},
+      requirements=['casa-python','casa-data','astropy','scipy','psrdada-python','h5py','numba'],
       zip_safe=False)
 
