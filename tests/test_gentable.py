e96b9e9ea7c61fb8e4fef31d9b344af548a9de68
Dana Simard <dsa@github.com>
2020-08-29 00:19:00 +0000

    test setup

--- tests/test_gentable.py
+++ tests/test_gentable.py
@@ -1,10 +1,14 @@
+import os
 import pytest
-from dsamfs import fringestopping
 from antpos import utils
+from dsamfs import fringestopping
+import numpy as np
 
-
-def test_gentable():
-    df_bls = fringestopping.get_baselines(antenna_order,autocorrs=True,casa_order=False)
-    blen = np.array([df_bls['x_m'],df_bls['y_m'],df_bls['z_m']]).T
-    utils.generate_fringestopping_table(blen,nint,outname=fs_table)
-    assert os.path.exists(fs_table)                   
+def test_gentable(tmpdir):
+    fstable = '{0}/fs_table.npz'.format(tmpdir)
+    nint = 10
+    antenna_order = [24, 10, 3, 66]
+    df_bls = utils.get_baselines(antenna_order, autocorrs=True, casa_order=False)
+    blen = np.array([df_bls['x_m'], df_bls['y_m'], df_bls['z_m']]).T
+    fringestopping.generate_fringestopping_table(blen, nint, outname=fstable)
+    assert os.path.exists(fstable)                   
