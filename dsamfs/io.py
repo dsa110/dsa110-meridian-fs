33c10c0376a6b029de9193bb36c4a8c2e6e47d1b
Dana Simard <dana.simard@astro.caltech.edu>
2021-02-18 15:01:31 -0800

    fix bug in delay writing

--- dsamfs/io.py
+++ dsamfs/io.py
@@ -124,10 +124,10 @@ def initialize_uvh5_file(fhdf, nfreq, npol, pt_dec, antenna_order, fobs,
     extra["phase_center_epoch"] = 2000
     if fs_table is not None:
         extra["fs_table"] = np.string_(fs_table)
-        snapdelays = pu.get_delays(np.array(antenna_order), nants_telescope)
-        extra["applied_delays_ns"] = np.string_(
-            ' '.join([str(d) for d in snapdelays.flatten()])
-        )
+    snapdelays = pu.get_delays(np.array(antenna_order), nants_telescope)
+    extra["applied_delays_ns"] = np.string_(
+        ' '.join([str(d) for d in snapdelays.flatten()])
+    )
     # Data sets
     data.create_dataset(
         "visdata", (0, 1, nfreq, npol), maxshape=(None, 1, nfreq, npol),
