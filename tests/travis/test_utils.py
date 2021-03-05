229bc1d8d349cc3ba1e8ee30b43cdec421106729
Dana Simard <dana.simard@astro.caltech.edu>
2021-03-05 04:38:40 +0000

    test for parsing params from dsautils.cnf

---
+++ tests/travis/test_utils.py
@@ -0,0 +1,8 @@
+from dsamfs.utils import parse_params
+
+def test_parse_params():
+    test, key_string, nant, nchan_spw, npol, fobs, \
+    samples_per_frame, samples_per_frame_out, nint, \
+    nfreq_int, antenna_order, pt_dec, tsamp, fringestop = parse_params()
+    assert nant == len(antenna_order)
+    
